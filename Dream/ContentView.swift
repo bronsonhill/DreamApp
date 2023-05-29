fbc3ee99131700db23de1af4d0e9b98344564f48
Bronson <bronson.hill@yahoo.com.au>
2023-05-29 22:07:32 +1000

    Initial Commit

---
+++ Dream/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Dream
+//
+//  Created by Bronson Hill on 29/5/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dream/DreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  DreamApp.swift
+//  Dream
+//
+//  Created by Bronson Hill on 29/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct DreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
